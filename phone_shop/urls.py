624f56773ce107f76c61e403d3ee1204bf58d31f
rz <kingradomir@gmail.com>
2023-03-25 21:16:20 +0600

    CRUD

--- phone_shop/urls.py
+++ phone_shop/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('phone_list/', views.phone_list_view, name='phone_list'),
     path('phone_list/<int:id>/', views.phone_detail_view, name='phone_detail'),
+    path('phone_list/<int:id>/delete/', views.delete_object_view, name='delete'),
+    path('phone_list/<int:id>/update/', views.update_object_view, name='update'),
+    path('create_phone/', views.create_object_view, name='create'),
 ]
--- phone_shop/views.py
+++ phone_shop/views.py
@@ -1,5 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-from . import models
+from . import models, forms
 
 #не полная информация о товаре
 def phone_list_view(request):
@@ -12,3 +13,37 @@ def phone_detail_view(request, id):
     phone_detail = get_object_or_404(models.PhoneShop, id=id)
     return render(request, 'phone_detail.html', {'phone_detail': phone_detail})
 
+#создание объектов через формы
+def create_object_view(request):
+    method = request.method
+    if method == "POST":
+        form = forms.PhoneForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Успешно добавлен в Базу Данных")
+    else:
+        form = forms.PhoneForm()
+    return render(request, "create_phone.html", {'form': form})
+
+#Удаление из базы
+def delete_object_view(request, id):
+    phone_object = get_object_or_404(models.PhoneShop, id=id)
+    phone_object.delete()
+    return HttpResponse('Телефон удален из Базы данных')
+
+#Редактирование
+def update_object_view(request, id):
+    phone_object = get_object_or_404(models.PhoneShop, id=id)
+    if request.method == 'POST':
+        form = forms.PhoneForm(instance=phone_object, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Данные успешно обновлены')
+    else:
+        form = forms.PhoneForm(instance=phone_object)
+
+    context = {
+        'form': form,
+        'object': phone_object
+    }
+    return render(request, 'update_phone.html', context)
